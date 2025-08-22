8c738f723e1d723a0edb2c41e3613f4176e31f16
HARIKRISHNA75 <harikrishnachowdam75@gmail.com>
2025-08-22 21:30:24 +0530

    Add files via upload

---
+++ CSS/krishna.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn dataset
+from sklearn.decomposition import PCA
+iris=pd.read_csv("iris.cvs")
+iris.head()
+iris.describe()
+iris.shape
